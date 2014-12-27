69ec5bb803f47b2edf819eb1cb341196539b623a
Abdul Wahab <arwahab@users.noreply.github.com>
2014-12-27 16:03:27 -0500

    Create SecondBase.java

---
+++ SecondBase.java
@@ -0,0 +1,14 @@
+package lab.pkg3;
+
+// Lab 3 //
+// Abdul Wahab //
+
+public class SecondBase extends FirstBase {
+
+    private String name;
+
+    public SecondBase() {
+        string = "Second Base";
+    }
+
+}
