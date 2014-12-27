33a6b8195992205c4d61dcb307f8b4038a293fcc
Abdul Wahab <arwahab@users.noreply.github.com>
2014-12-27 16:04:14 -0500

    Create BaseClass.java

---
+++ BaseClass.java
@@ -0,0 +1,18 @@
+package lab.pkg3;
+
+// Lab 3 //
+// Abdul Wahab //
+
+public class BaseClass {
+
+    public String string;
+
+    BaseClass() {
+        string = "Base Class";
+    }
+
+    public String Only() {
+        return string;
+    }
+
+}
