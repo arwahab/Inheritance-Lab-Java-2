c69e6f751c86f9268ef1b072ae9d5da67db4d898
Abdul Wahab <arwahab@users.noreply.github.com>
2014-12-27 16:02:47 -0500

    Create FirstBase.java

---
+++ FirstBase.java
@@ -0,0 +1,20 @@
+package lab.pkg3;
+
+//Write a program such that the SecondBase class inherits (extends) from the FirstBase class and the First Base class inherits from (extends) the Baseclass.   In other words the Baseclass is the parent of FirstBase and First Base is the parent of SecondBase.
+//In addition add a method in your main class that accepts only the Baseclass as an argument.  
+//Instantiate the three classes in your main class, and pass each one to main method you added in step 2 above.   Each object that you pass to the method should enable the method to print its name.
+
+// Lab 3 //
+// Abdul Wahab //
+    
+class FirstBase extends BaseClass {
+
+public FirstBase(){
+    string = "First Base";
+}
+        
+ 
+
+
+
+}
